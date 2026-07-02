a51cec8859105d87f83d82d321d59acd1edd151b
kambiun2025-cpu <kambiun2025@gmail.com>
2026-07-02 16:42:44 +0200

    Arranque proyecto
    Carpeta Hijo

---
+++ salient-child/functions.php
@@ -0,0 +1,24 @@
+<?php 
+
+add_action( 'wp_enqueue_scripts', 'salient_child_enqueue_styles', 100);
+
+function salient_child_enqueue_styles() {
+		
+		$nectar_theme_version = nectar_get_theme_version();
+		wp_enqueue_style( 'salient-child-style', get_stylesheet_directory_uri() . '/style.css', '', $nectar_theme_version );
+		
+    if ( is_rtl() ) {
+   		wp_enqueue_style(  'salient-rtl',  get_template_directory_uri(). '/rtl.css', array(), '1', 'screen' );
+		}
+}
+
+
+add_action('wp_head', function () {
+  if (is_page([251, 231, 3])) {
+    echo '<meta name="robots" content="noindex, nofollow">';
+  }
+});
+
+
+
+?>
